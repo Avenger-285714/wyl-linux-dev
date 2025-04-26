deepin-community/kernel

Revert "irqchip/loongson-eiointc: fix gsi register error"
This reverts commit 16b46d963210d7dbd0bfc9d3a46c99681756414a.

drivers/irqchip/irq-loongson-eiointc.c
@@ -375,7 +375,7 @@ int __init pch_msi_parse_madt(union acpi_subtable_headers *header,
 	int node;
 
 	if (cpu_has_flatmode)
-		node = eiointc_priv[nr_pics - 1]->node / NODES_PER_FLATMODE_NODE;
+		node = cpu_to_node(eiointc_priv[nr_pics - 1]->node * CORES_PER_EIO_NODE);
 	else
 		node = eiointc_priv[nr_pics - 1]->node;
 
@@ -478,7 +478,7 @@ int __init eiointc_acpi_init(struct irq_domain *parent,
 		goto out_free_handle;
 
 	if (cpu_has_flatmode)
-		node = acpi_eiointc->node / NODES_PER_FLATMODE_NODE;
+		node = cpu_to_node(acpi_eiointc->node * CORES_PER_EIO_NODE);
 	else
 		node = acpi_eiointc->node;
 	acpi_set_vec_parent(node, priv->eiointc_domain, pch_group);

Avenger-285714/wyl-linux-dev

Signed-off-by: WangYuli <wangyuli@uniontech.com>

drivers/irqchip/irq-loongson-eiointc.c
@@ -376,7 +376,7 @@ int __init pch_msi_parse_madt(union acpi_subtable_headers *header,
 	int node;
 
 	if (cpu_has_flatmode)
-		node = eiointc_priv[nr_pics - 1]->node / NODES_PER_FLATMODE_NODE;
+		node = early_cpu_to_node(eiointc_priv[nr_pics - 1]->node * CORES_PER_EIO_NODE);
 	else
 		node = eiointc_priv[nr_pics - 1]->node;
 
@@ -479,7 +479,7 @@ int __init eiointc_acpi_init(struct irq_domain *parent,
 		goto out_free_handle;
 
 	if (cpu_has_flatmode)
-		node = acpi_eiointc->node / NODES_PER_FLATMODE_NODE;
+		node = early_cpu_to_node(acpi_eiointc->node * CORES_PER_EIO_NODE);
 	else
 		node = acpi_eiointc->node;
 	acpi_set_vec_parent(node, priv->eiointc_domain, pch_group);